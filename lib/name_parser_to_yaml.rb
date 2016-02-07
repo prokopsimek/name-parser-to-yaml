d22c7de81e12d335585ffa0c1f87500eac0e5213
Prokop Simek <prokop.simek@applifting.cz>
2016-02-08 00:28:31 +0100

    get from another website

--- lib/name_parser_to_yaml.rb
+++ lib/name_parser_to_yaml.rb
@@ -2,6 +2,7 @@ require "name_parser_to_yaml/version"
 require 'open-uri'
 require 'nokogiri'
 require 'active_support/all'
+require 'yaml'
 
 module NameParserToYaml
 
@@ -52,15 +53,17 @@ module NameParserToYaml
     end
 
     def parse_names_from_country(parsed_nokogiri_body)
-      names_hash = []
+      names_hash = {}
       puts ""
       print "Parsing name: "
       parsed_nokogiri_body.css('.body .browsename').each do |row_with_name|
         name = row_with_name.css('b a').first.content
         name = name.split(' ').first
-        print "#{name}, "
+        print "#{name}"
         gender = row_with_name.css('span.fem, span.masc').first.content
-        names_hash << { "#{name.to_s.mb_chars.downcase.to_s.humanize}" => {'gender' => gender.to_s, 'name_day' => get_name_day(name.parameterize)} }
+        name_day = get_name_day(name.parameterize)
+        print " on #{name_day}, "
+        names_hash["#{name.to_s.mb_chars.downcase.to_s.humanize}"] = {'gender' => gender.to_s, 'name_day' => name_day}
       end
       names_hash
     end
@@ -71,12 +74,52 @@ module NameParserToYaml
       country_href = hrefs.select{ |a| a.content.parameterize.include?(country.parameterize) }.first
       if country_href
         text_after = country_href.next.content.gsub(': ', '')
-        formatted = DateTime.civil_from_format(:local, text_after.split(' ').second.to_i, Date::MONTHNAMES.index("#{text_after.split(' ').first}"))
-        return formatted.to_date.to_formatted_s(:short).to_s[1..-1]
+        if !text_after.blank?
+          formatted = DateTime.civil_from_format(:local, text_after.split(' ').second.to_i, Date::MONTHNAMES.index("#{text_after.split(' ').first}"))
+          return formatted.to_date.to_formatted_s(:short).to_s[1..-1]
+        else
+          return nil
+        end
+      else
+        return get_name_day_from_nameday_com(name)
+        # return nil
+      end
+    end
+
+    def get_name_day_from_nameday_com(name)
+      first_letter = name.first.parameterize.downcase
+      url = "http://www.mynameday.com/#{first_letter}.html"
+      parsed_nokogiri_body = parse_with_nokogiri(url)
+      found_name = parsed_nokogiri_body.css('table.excel9 td').select{ |td| td.content.parameterize.downcase.include?(name.parameterize.downcase)}
+      if found_name.first
+        found_name_name_day = found_name.first.previous_element.previous_element
+        return found_name_name_day.content.gsub('-', ' ')
       else
         return nil
       end
     end
+
+#     def determine_names_days
+#       yaml = YAML.load_file(country + '.yml')
+#       yaml = yaml.to_a
+#
+#       file = File.join(country + '1.yml')
+#       File.delete(file) if File.exist?(file)
+#
+#       p yaml.first(3)
+#       yaml.first(3).each do |hash|
+#         p hash
+#         p hash[0].key
+#         p hash['name_day']
+#         p hash.first
+#         hash['name_day'] = get_name_day_from_nameday_com(hash.key)
+# p hash
+#       end
+#
+#
+#
+#       File.open(file, 'w') { |file| file.write(yaml.to_yaml) }
+#     end
 
   end
 end

1ac226a0f89a2b02b4fb1bcc73946eabf481f034
Prokop Simek <prokop.simek@applifting.cz>
2016-02-07 22:00:38 +0100

    added logic

--- lib/name_parser_to_yaml.rb
+++ lib/name_parser_to_yaml.rb
@@ -1,5 +1,75 @@
 require "name_parser_to_yaml/version"
+require 'open-uri'
 
 module NameParserToYaml
-  # Your code goes here...
+
+  class Parser
+
+    attr_reader :country
+
+    PARSE_URL = 'http://www.behindthename.com'
+
+    def initialize(country)
+      @country = country # array like 'english', 'czech', etc.
+    end
+
+    def generate!
+      names_in_country = get_all_names_in_country
+      save_names_to_yaml!(names_in_country)
+    end
+
+    def get_all_names_in_country
+      country_url = get_country_url
+      all_names = []
+
+      4.times do |i|
+        parsed_nokogiri_body = parse_with_nokogiri(country_url + '/' + i.to_s)
+        all_names << parse_names_from_country(parsed_nokogiri_body)
+      end
+
+      all_names.flatten
+    end
+
+    def save_names_to_yaml!(country_names)
+      yamled = country_names.to_yaml
+
+      file = File.join(country + '.yml')
+      file.delete! rescue ''
+      File.open(file, 'w') { |file| file.write(yamled) }
+    end
+
+
+    def get_country_url
+      PARSE_URL + '/names/usage/' + country
+    end
+
+    def parse_with_nokogiri(url)
+      body = Nokogiri::HTML(open(url))
+    end
+
+    def parse_names_from_country(parsed_nokogiri_body)
+      names_hash = []
+      parsed_nokogiri_body.css('.body .browsename').each do |row_with_name|
+        name = row_with_name.css('b a').first.content
+        name = name.split(' ').first
+        gender = row_with_name.css('span.fem, span.masc').first.content
+        names_hash << { "#{name.to_s.mb_chars.downcase.to_s.humanize}" => {'gender' => gender.to_s, 'name_day' => get_name_day(name.parameterize)} }
+      end
+      names_hash
+    end
+
+    def get_name_day(name)
+      # , name_day: get_name_day(name.parameterize)
+      parsed_body = parse_with_nokogiri(PARSE_URL + '/name/' + name + '/namedays')
+      hrefs = parsed_body.css('.body a')
+      country_href = hrefs.select{ |a| a.content.parameterize.include?(country.parameterize) }.first
+      if country_href
+        text_after = country_href.next.content.gsub(': ', '')
+        return DateTime.civil_from_format(:local, text_after.split(' ').second.to_i, Date::MONTHNAMES.index("#{text_after.split(' ').first}")).to_date
+      else
+        return nil
+      end
+    end
+
+  end
 end
--- name_parser_to_yaml.gemspec
+++ name_parser_to_yaml.gemspec
@@ -9,9 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Prokop Simek"]
   spec.email         = ["prokop.simek@applifting.cz"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Name and their name days parser to yaml.}
+  spec.description   = %q{Gem parses names and their name days from server behindname.com and saves them formatted into yaml.}
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
@@ -26,6 +25,10 @@ Gem::Specification.new do |spec|
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
+
+  spec.add_dependency "httparty"
+  spec.add_dependency "nokogiri"
+  spec.add_dependency "open-uri"
 
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
